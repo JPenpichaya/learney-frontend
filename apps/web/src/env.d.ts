f83bd4e5f30c37ca81ebecd7238ecf2983c03a9a
JPenpichaya <j.penpichaya@gmail.com>
2025-11-08 14:13:31 +0700

    fix bug

--- apps/web/src/env.d.ts
+++ apps/web/src/env.d.ts
@@ -1,8 +1,10 @@
 interface ImportMetaEnv {
+  readonly VITE_API_URL: string
+  readonly VITE_STRIPE_PUBLISHABLE_KEY_TEST: string
+  readonly VITE_GCP_API_KEY: string
+  readonly VITE_GCP_AUTH_DOMAIN: string
+  readonly VITE_GCP_PROJECT_ID: string
   readonly VITE_API_BASE: string
-  readonly VITE_LIVEKIT_URL?: string
-  readonly VITE_LIVEKIT_KEY?: string
-  // add more VITE_ vars as needed
 }
 
 interface ImportMeta {
