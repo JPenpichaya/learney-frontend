9e0cf412c8ac37395f6b1fe99901e0c479af2c1b
JPenpichaya <j.penpichaya@gmail.com>
2025-12-15 11:58:59 +0700

    cloud set up

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8080',
+        target: process.env.API_BASE_URL,
         changeOrigin: true,
       },
     },
