aad35984654581b9514d0d2c2416e8073d2f3fdd
JPenpichaya <j.penpichaya@gmail.com>
2025-12-20 15:55:18 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -38,7 +38,12 @@ export default function App() {
           {/* หน้า Login */}
           <Route path="/login" element={<Login />} />
 
-          <Route path="/coursesPage" element={<CoursePage />} />
+          <Route path="/courses"
+            element={
+              <RequireAuth>
+                <CoursePage />
+              </RequireAuth>
+            } />
 
           <Route path="/call" element={<CallPage identity="anonymous" />} />
           <Route path="/live" element={<LiveRoomPage />} />
